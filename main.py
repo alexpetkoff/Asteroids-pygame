52c5d9ae6650bf334d8b220217fa1d96d220a069
alexpetkoff <iskander9207@icloud.com>
2025-09-03 14:14:00 +0300

    add drawables and updatable groups

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from player import *
 def main():
     pygame.init()
 
-
     clock = pygame.time.Clock()
     dt = 0
 
@@ -14,24 +13,28 @@ def main():
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
 
-    player = Player(x, y)
+    updatable = []
+    drawable = []
 
-    while(True):
+    player = Player(x, y)
+    updatable.append(player)
+    drawable.append(player)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        player.update(dt)
+
+        for up in updatable:
+            up.update(dt)
 
         screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        for draw in drawable:
+            draw.draw(screen)
+
         pygame.display.flip()
-
         dt = clock.tick(60) / 1000
-
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
