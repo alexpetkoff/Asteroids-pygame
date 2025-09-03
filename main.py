36ac06f3e8072f832d55e026696ee24d5725328e
alexpetkoff <iskander9207@icloud.com>
2025-09-03 13:55:23 +0300

    add forward and backward movement for the space ship

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
-    dt = clock.tick(60)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
@@ -21,10 +20,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
 
         screen.fill((0, 0, 0))
         player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
     # print("Starting Asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
